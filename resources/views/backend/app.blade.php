c16412b049196355421b53d1d0e031ea5b501a96
novianh <noviponorogo3@gmail.com>
2022-06-17 11:44:56 +0700

    integrasi dashboard template with laravel

---
+++ resources/views/backend/app.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Skydash Admin</title>
+    @include('backend.layouts.style')
+    @yield('style')
+    <link rel="shortcut icon" href="images/favicon.png" />
+</head>
+
+<body>
+    <div class="container-scroller">
+        <!-- partial:partials/_navbar.html -->
+        @include('backend.layouts.navbar')
+        <!-- partial -->
+        <div class="container-fluid page-body-wrapper">
+            <!-- partial:partials/_settings-panel.html -->
+            @include('backend.layouts.settings-panel')
+            <!-- partial -->
+            <!-- partial:partials/_sidebar.html -->
+            @include('backend.layouts.sidebar')
+            <!-- partial -->
+            <div class="main-panel">
+                <div class="content-wrapper">
+                  <h2 class="mb-5">
+                    @yield('title')
+                  </h2>
+                    @yield('content')
+                </div>
+                <!-- content-wrapper ends -->
+                <!-- partial:partials/_footer.html -->
+                @include('backend.layouts.footer')
+                <!-- partial -->
+            </div>
+            <!-- main-panel ends -->
+        </div>
+        <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+
+    <!-- plugins:js -->
+    @include('backend.layouts.javascript')
+    @yield('js')
+    <!-- End custom js for this page-->
+</body>
+
+</html>
---
+++ resources/views/backend/layouts/style.blade.php
@@ -0,0 +1,15 @@
+   <!-- plugins:css -->
+   <link rel="stylesheet" href="{{ asset('template/vendors/feather/feather.css') }}">
+   <link rel="stylesheet" href="{{ asset('template/vendors/ti-icons/css/themify-icons.css') }}">
+   <link rel="stylesheet" href="{{ asset('template/vendors/css/vendor.bundle.base.css') }}">
+   <!-- endinject -->
+   <!-- Plugin css for this page -->
+   <link rel="stylesheet" href="{{ asset('template/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
+   <link rel="stylesheet" href="{{ asset('template/vendors/ti-icons/css/themify-icons.css') }}">
+   <link rel="stylesheet" type="text/css" href="{{ asset('template/js/select.dataTables.min.css') }}">
+   <!-- End plugin css for this page -->
+   <!-- inject:css -->
+   <link rel="stylesheet" href="{{ asset('template/css/vertical-layout-light/style.css') }}">
+   <!-- endinject -->
+   <!-- CSS only -->
+
